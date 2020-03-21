7374eb8fcf01046c8a11a3a60f48ab4ac602035f
Bruno Chaves <brunochavesg@gmail.com>
2020-03-21 00:51:08 -0300

    testegit.py

---
+++ testegit.py
@@ -0,0 +1,16 @@
+nome = input ("Qual seu nome?: ")
+print ("Ola " +nome, "Seja Bem-vindo")
+
+dia = input("Qual o Dia que você nasceu?: ")
+mes = input ("E o mês?: ")
+ano = input ("Agora o Ano!: ")
+
+print("Você nasceu dia " +dia, "do mês de " +mes, "e do ano de " +ano,)
+
+n1 = int(input ("Irei somar um numero para você, me fala o primeiro numero: "))
+n2 = int(input("Agora me fala o segundo numero: "))
+
+print ("Resultado ", (n1+n2))
+
+import os
+os.system("pause")
